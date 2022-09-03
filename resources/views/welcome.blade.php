0b6acda97cd5673fd887c08e82d7fde32f33944f
royhan76 <beensholih@gmail.com>
2022-09-03 21:36:57 +0700

    add page maintace

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,43 +1,259 @@
-<!doctype html>
-<title>Site Maintenance</title>
-<style>
-    body {
-        text-align: center;
-        padding: 150px;
-    }
+<!DOCTYPE html>
+<html lang="en-US">
 
-    h1 {
-        font-size: 50px;
-    }
+<head>
+    <script src="//archive.org/includes/analytics.js?v=cf34f82" type="text/javascript"></script>
+    <script type="text/javascript">
+        window.addEventListener('DOMContentLoaded', function() {
+            var v = archive_analytics.values;
+            v.service = 'wb';
+            v.server_name = 'wwwb-app223.us.archive.org';
+            v.server_ms = 284;
+            archive_analytics.send_pageview({});
+        });
+    </script>
+    <script type="text/javascript" src="/_static/js/bundle-playback.js?v=36gO9Ebf" charset="utf-8"></script>
+    <script type="text/javascript" src="/_static/js/wombat.js?v=UHAOicsW" charset="utf-8"></script>
+    <script type="text/javascript">
+        __wm.init("https://web.archive.org/web");
+        __wm.wombat("https://ups-error.com/", "20220511025339", "https://web.archive.org/", "web", "/_static/",
+            "1652237619");
+    </script>
+    <link rel="stylesheet" type="text/css" href="/_static/css/banner-styles.css?v=fantwOh2" />
 
-    body {
-        font: 20px Helvetica, sans-serif;
-        color: #333;
-    }
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    article {
-        display: block;
-        text-align: left;
-        width: 650px;
-        margin: 0 auto;
-    }
+    <title>Under Maintance</title>
 
-    a {
-        color: #dc8100;
-        text-decoration: none;
-    }
+    <link rel="dns-prefetch" href="//web.archive.org/web/20220511025339/https://s.w.org/">
 
-    a:hover {
-        color: #333;
-        text-decoration: none;
-    }
-</style>
 
-<article>
-    <h1>We&rsquo;ll be back soon!</h1>
-    <div>
-        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can
-            always <a href="mailto:#">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
-        <p>&mdash; The Team</p>
-    </div>
-</article>
+
+    <link rel="stylesheet" id="wp-block-library-css"
+        href="https://web.archive.org/web/20220511025339cs_/https://ups-error.com/wp-includes/css/dist/block-library/style.min.css"
+        type="text/css" media="all">
+    <link rel="stylesheet" id="hello-elementor-css"
+        href="https://web.archive.org/web/20220511025339cs_/https://ups-error.com/wp-content/themes/hello-elementor/style.min.css"
+        type="text/css" media="all">
+    <link rel="stylesheet" id="hello-elementor-theme-style-css"
+        href="https://web.archive.org/web/20220511025339cs_/https://ups-error.com/wp-content/themes/hello-elementor/theme.min.css"
+        type="text/css" media="all">
+    <link rel="stylesheet" id="elementor-icons-css"
+        href="https://web.archive.org/web/20220511025339cs_/https://ups-error.com/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css"
+        type="text/css" media="all">
+    <link rel="stylesheet" id="elementor-animations-css"
+        href="https://web.archive.org/web/20220511025339cs_/https://ups-error.com/wp-content/plugins/elementor/assets/lib/animations/animations.min.css"
+        type="text/css" media="all">
+    <link rel="stylesheet" id="elementor-frontend-css"
+        href="https://web.archive.org/web/20220511025339cs_/https://ups-error.com/wp-content/plugins/elementor/assets/css/frontend.min.css"
+        type="text/css" media="all">
+    <link rel="stylesheet" id="elementor-global-css"
+        href="https://web.archive.org/web/20220511025339cs_/https://ups-error.com/wp-content/uploads/elementor/css/global.css"
+        type="text/css" media="all">
+    <link rel="stylesheet" id="elementor-post-2-css"
+        href="https://web.archive.org/web/20220511025339cs_/https://ups-error.com/wp-content/uploads/elementor/css/post-2.css"
+        type="text/css" media="all">
+    <link rel="stylesheet" id="google-fonts-1-css"
+        href="https://web.archive.org/web/20220511025339cs_/httpss://fonts.googleapis.com/css?family=Roboto%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto+Slab%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CMontserrat%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&amp;ver=5.2.3"
+        type="text/css" media="all">
+    <link rel="stylesheet" id="elementor-icons-shared-0-css"
+        href="https://web.archive.org/web/20220511025339cs_/https://ups-error.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/fontawesome.min.css"
+        type="text/css" media="all">
+    <link rel="stylesheet" id="elementor-icons-fa-solid-css"
+        href="https://web.archive.org/web/20220511025339cs_/https://ups-error.com/wp-content/plugins/elementor/assets/lib/font-awesome/css/solid.min.css"
+        type="text/css" media="all">
+
+</head>
+
+<body class="home page-template-default page page-id-2 elementor-default elementor-page elementor-page-2">
+
+    <header class="site-header" role="banner">
+
+        <div class="site-branding">
+        </div>
+
+    </header>
+
+    <main class="site-main post-2 page type-page status-publish hentry" role="main">
+        <header class="page-header">
+        </header>
+        <div class="page-content">
+            <div data-elementor-type="wp-page" data-elementor-id="2" class="elementor elementor-2"
+                data-elementor-settings="[]">
+                <div class="elementor-inner">
+                    <div class="elementor-section-wrap">
+                        <section
+                            class="elementor-element elementor-element-3bb39209 elementor-section-content-middle elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section"
+                            data-id="3bb39209" data-element_type="section">
+                            <div class="elementor-container elementor-column-gap-default">
+                                <div class="elementor-row">
+                                    <div class="elementor-element elementor-element-3a2d379c elementor-column elementor-col-50 elementor-top-column"
+                                        data-id="3a2d379c" data-element_type="column">
+                                        <div class="elementor-column-wrap  elementor-element-populated">
+                                            <div class="elementor-widget-wrap">
+                                                <div class="elementor-element elementor-element-479ef8c8 elementor-widget elementor-widget-heading"
+                                                    data-id="479ef8c8" data-element_type="widget"
+                                                    data-widget_type="heading.default">
+                                                    <div class="elementor-widget-container">
+                                                        {{-- <h2 class="elementor-heading-title elementor-size-default">Oops
+                                                            Error...</h2>
+                                                        <br> --}}
+                                                        <h2 class="elementor-heading-title elementor-size-default">This
+                                                            Site Under Maintace...</h2>
+                                                    </div>
+                                                </div>
+                                                <div class="elementor-element elementor-element-3d67c8e2 elementor-widget elementor-widget-text-editor"
+                                                    data-id="3d67c8e2" data-element_type="widget"
+                                                    data-widget_type="text-editor.default">
+                                                    <div class="elementor-widget-container">
+                                                        <div class="elementor-text-editor elementor-clearfix">
+                                                            <p>The Page You Are Looking For Couldn’t Be Found. <br>You
+                                                                are experiencing technical issues. Please contact our
+                                                                support to get more information.</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="elementor-element elementor-element-8fb3ecc elementor-align-left elementor-widget elementor-widget-button"
+                                                    data-id="8fb3ecc" data-element_type="widget"
+                                                    data-widget_type="button.default">
+                                                    <div class="elementor-widget-container">
+                                                        <div class="elementor-button-wrapper">
+                                                            <a href="https://wa.me/6285742525351"
+                                                                class="elementor-button-link elementor-button elementor-size-md"
+                                                                rel="nofollow" role="button">
+                                                                <span class="elementor-button-content-wrapper">
+                                                                    <span
+                                                                        class="elementor-button-icon elementor-align-icon-right">
+                                                                    </span>
+                                                                    <span class="elementor-button-text">Contact
+                                                                        Support</span>
+                                                                </span>
+                                                            </a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="elementor-element elementor-element-31e06d3e elementor-column elementor-col-50 elementor-top-column"
+                                        data-id="31e06d3e" data-element_type="column">
+                                        <div class="elementor-column-wrap  elementor-element-populated">
+                                            <div class="elementor-widget-wrap">
+                                                <div class="elementor-element elementor-element-5f50dd42 elementor-widget elementor-widget-image"
+                                                    data-id="5f50dd42" data-element_type="widget"
+                                                    data-widget_type="image.default">
+                                                    <div class="elementor-widget-container">
+                                                        <div class="elementor-image">
+                                                            <img width="550" height="550"
+                                                                src="https://web.archive.org/web/20220511025339im_/https://ups-error.com/wp-content/uploads/2019/09/Error-Illustrasi-01.png"
+                                                                class="attachment-large size-large" alt="error page"
+                                                                srcset="https://web.archive.org/web/20220511025339im_/https://ups-error.com/wp-content/uploads/2019/09/Error-Illustrasi-01.png 550w, https://web.archive.org/web/20220511025339im_/https://ups-error.com/wp-content/uploads/2019/09/Error-Illustrasi-01-150x150.png 150w, https://web.archive.org/web/20220511025339im_/https://ups-error.com/wp-content/uploads/2019/09/Error-Illustrasi-01-300x300.png 300w"
+                                                                sizes="(max-width: 550px) 100vw, 550px">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+            <div class="post-tags">
+            </div>
+        </div>
+
+        <section id="comments" class="comments-area">
+
+
+
+
+        </section>
+    </main>
+
+    <footer id="site-footer" class="site-footer" role="contentinfo">
+    </footer>
+
+    <script type="text/javascript"
+        src="https://web.archive.org/web/20220511025339js_/https://ups-error.com/wp-includes/js/jquery/jquery.js"></script>
+    <script type="text/javascript"
+        src="https://web.archive.org/web/20220511025339js_/https://ups-error.com/wp-includes/js/jquery/jquery-migrate.min.js">
+    </script>
+    <script type="text/javascript"
+        src="https://web.archive.org/web/20220511025339js_/https://ups-error.com/wp-content/plugins/elementor/assets/js/frontend-modules.min.js">
+    </script>
+    <script type="text/javascript"
+        src="https://web.archive.org/web/20220511025339js_/https://ups-error.com/wp-includes/js/jquery/ui/position.min.js">
+    </script>
+    <script type="text/javascript"
+        src="https://web.archive.org/web/20220511025339js_/https://ups-error.com/wp-content/plugins/elementor/assets/lib/dialog/dialog.min.js">
+    </script>
+    <script type="text/javascript"
+        src="https://web.archive.org/web/20220511025339js_/https://ups-error.com/wp-content/plugins/elementor/assets/lib/waypoints/waypoints.min.js">
+    </script>
+    <script type="text/javascript"
+        src="https://web.archive.org/web/20220511025339js_/https://ups-error.com/wp-content/plugins/elementor/assets/lib/swiper/swiper.min.js">
+    </script>
+    <script type="text/javascript">
+        var elementorFrontendConfig = {
+            "environmentMode": {
+                "edit": false,
+                "wpPreview": false
+            },
+            "is_rtl": false,
+            "breakpoints": {
+                "xs": 0,
+                "sm": 480,
+                "md": 768,
+                "lg": 1025,
+                "xl": 1440,
+                "xxl": 1600
+            },
+            "version": "2.7.1",
+            "urls": {
+                "assets": "https:\/\/web.archive.org\/web\/20220511025339\/https:\/\/ups-error.com\/wp-content\/plugins\/elementor\/assets\/"
+            },
+            "settings": {
+                "page": [],
+                "general": {
+                    "elementor_global_image_lightbox": "yes",
+                    "elementor_enable_lightbox_in_editor": "yes"
+                }
+            },
+            "post": {
+                "id": 2,
+                "title": "",
+                "excerpt": ""
+            }
+        };
+    </script>
+    <script type="text/javascript"
+        src="https://web.archive.org/web/20220511025339js_/https://ups-error.com/wp-content/plugins/elementor/assets/js/frontend.min.js">
+    </script>
+
+</body>
+
+</html>
+<!--
+     FILE ARCHIVED ON 02:53:39 May 11, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 07:23:11 Jun 07, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+-->
+<!--
+playback timings (ms):
+  captures_list: 222.902
+  exclusion.robots: 0.189
+  exclusion.robots.policy: 0.183
+  RedisCDXSource: 0.613
+  esindex: 0.009
+  LoadShardBlock: 62.239 (3)
+  PetaboxLoader3.datanode: 59.56 (4)
+  CDXLines.iter: 30.098 (3)
+  load_resource: 41.175
+-->
